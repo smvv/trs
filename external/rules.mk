93a5449d9e6f911d93050a6773a2d4f1be85c948
Sander Mathijs van Veen <smvv@kompiler.org>
2011-11-19 03:58:28 +0100

    Enable optimized bison generated parser builds.

--- external/rules.mk
+++ external/rules.mk
@@ -5,13 +5,13 @@ PYBISON_INC := -Iexternal/pybison/src/c -I/usr/include/python2.7
 build: $(b)pybison/bison_.so $(b)pybison/bison.py
 
 $(b)pybison/bison_.so: $(b)pybison/bison_.o $(b)pybison/bisondynlib-linux.o
-	$(CC) -g -O0 -pipe -Wall -Wextra -shared -pthread -o $@ $^
+	$(CC) -g -O3 -pipe -Wall -Wextra -shared -pthread -o $@ $^
 
 $(b)pybison/bison.py: $(d)pybison/src/python/bison.py | $(b)pybison
 	ln -s `realpath $<` $@
 
 $(b)pybison/bisondynlib-linux.o $(b)pybison/bison_.o: | $(b)pybison
-	$(CC) -g -O0 -pipe -Wall -Wextra -o $@ -c $< -pthread -fPIC $(PYBISON_INC)
+	$(CC) -g -O3 -pipe -Wall -Wextra -o $@ -c $< -pthread -fPIC $(PYBISON_INC)
 
 $(b)pybison/bisondynlib-linux.o: $(d)pybison/src/c/bisondynlib-linux.c
 $(b)pybison/bison_.o: $(b)pybison/bison_.c
