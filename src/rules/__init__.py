dbd0b0907c8094da387bf73d3188323e139d1aa3
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-01-08 03:22:46 +0100

    Added powers to possibilities recognized in main execution.

--- src/rules/__init__.py
+++ src/rules/__init__.py
@@ -1,12 +1,14 @@
-from ..node import OP_ADD, OP_MUL
+from ..node import OP_ADD, OP_MUL, OP_DIV, OP_POW
 from .poly import match_combine_polynomes, match_expand
+from .powers import match_add_exponents, match_subtract_exponents, \
+        match_multiply_exponents, match_duplicate_exponent, \
+        match_remove_negative_exponent, match_exponent_to_root
 
 
 RULES = {
         OP_ADD: [match_combine_polynomes],
-        OP_MUL: [match_expand],
-        #OP_MUL: [match_expand, match_add_exponents],
-        #OP_DIV: [match_subtract_exponents],
-        #OP_POW: [match_multiply_exponents, match_duplicate_exponent, \
-        #         match_remove_negative_exponent, match_exponent_to_root],
+        OP_MUL: [match_expand, match_add_exponents],
+        OP_DIV: [match_subtract_exponents],
+        OP_POW: [match_multiply_exponents, match_duplicate_exponent, \
+                 match_remove_negative_exponent, match_exponent_to_root],
         }
