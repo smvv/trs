ab2c06a884afd75ce9314f1b6f5971c6ee1ad0d0
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-02-23 12:53:09 +0100

    simplified a unit test.

--- tests/test_rules_goniometry.py
+++ tests/test_rules_goniometry.py
@@ -6,12 +6,9 @@ from tests.rulestestcase import RulesTestCase, tree
 class TestRulesGoniometry(RulesTestCase):
 
     def test_match_add_quadrants(self):
-        return
         root = tree('sin(x) ^ 2 + cos(x) ^ 2')
         possibilities = match_add_quadrants(root)
         self.assertEqualPos(possibilities, [P(root, add_quadrants, ())])
 
     def test_add_quadrants(self):
-        return
-        root = tree('sin(x) ^ 2 + cos(x) ^ 2')
-        self.assertEqual(add_quadrants(root, ()), 1)
+        self.assertEqual(add_quadrants(None, ()), 1)
